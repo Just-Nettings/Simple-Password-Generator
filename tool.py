81f7b0faf5e5cd609585252d7b751336eb42b88c
Just-Nettings <140922829+Just-Nettings@users.noreply.github.com>
2023-07-29 14:39:59 -0800

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,8 @@
+import random
+
+print("password is:")
+chars = "abcdefghyjklmnopqrstuvwsynp1234567890!@#$&*()?"
+password = ""
+for x in range(16):
+    password += random.choice(chars)
+print(password)
